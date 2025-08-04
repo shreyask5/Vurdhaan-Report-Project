757b10e457f4cfb4d4458c1e52105a46d5c848ae
shreyask5 <shreyasksh5@gmail.com>
2025-08-04 11:40:18 +0530

    SQL Agent ChatBot

--- app4.py
+++ app4.py
@@ -1057,7 +1057,6 @@ if __name__ == '__main__':
         app.run(
             host='0.0.0.0',
             port=5001,
-            debug=Config.FLASK_ENV == 'development',
             use_reloader=False
         )
     except Exception as e:
